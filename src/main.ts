a5c98ecc8f7584301daac32a95edcd34370a90a9
zeyadsleem <zeyadsleem1@gmail.com>
2024-01-28 06:28:39 +0200

    setup: biome for liniting and formmating, setup tailwind for styling

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import './assets/main.css'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./styles.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+app.use(router);
+app.mount("#app");
